df35d5c1ff4fb8f2783789b36ea388dfafd34144
Mortyec <tony.anthony@hotmail.es>
2022-01-10 18:23:17 -0500

    first commit

---
+++ app/src/main/java/com/aperez/apps/androidfunwithflags/ADPP_SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.aperez.apps.androidfunwithflags;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ADPP_SettingsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adpp_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
---
+++ app/src/main/java/com/aperez/apps/androidfunwithflags/ADPP_SettingsActivityFragment.java
@@ -0,0 +1,17 @@
+package com.aperez.apps.androidfunwithflags;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ADPP_SettingsActivityFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
