1179c62f1160efe9ccdcff026a01b10ff0323fcc
Mortyec <tony.anthony@hotmail.es>
2022-01-10 19:14:15 -0500

    Anthony Perez 19:00

---
+++ app/src/main/java/com/aperez/apps/anthony_perez_2doparcial_prueba_01/ADPP_Activity_Cliente_Login.java
@@ -0,0 +1,13 @@
+package com.aperez.apps.anthony_perez_2doparcial_prueba_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ADPP_Activity_Cliente_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adpp_cliente_login);
+    }
+}
